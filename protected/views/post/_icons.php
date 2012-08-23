6acb0b2c8bcddef78de092742a5eeb15b04b98ff
unclepotap <unclepotap@gmail.com>
2012-08-23 19:18:02 +0400

    Устранение ошибок в _icons.php

--- protected/views/post/_icons.php
+++ protected/views/post/_icons.php
@@ -11,36 +11,36 @@
 <i class="icon-comment" rel="tooltip" title="число комментариев"><?php echo  $model->comment_count;?> </i>
 <i class="icon-eye-open" rel="tooltip" title="число просмотров"><?php echo  $model->view_count;?> </i>
 <?php 
-if (Post::inFavorite($model->id))
-{
-//если статья уже в избранном 
- 
-    echo CHtml::ajaxLink('<i class="icon-heart gray-a" 
-                                rel="tooltip" 
-                                title="убрать из избранного">'.$model->favourite_count.'</i>', 
-                Yii::app()->createUrl('post/favorite'), array(
-                'type' => 'POST',
-                'data' => "js:'id-post='+$model->id",
-                'success' => 'js:function(data) {postfavsuccess(data);}',
-                'error' => 'js:function(data) {posterror();}',
-            ));
-        } else { 
-//  если статья не в избранном
+if (Yii::app()->user->getId()) {
+    if (Post::inFavorite($model->id))
+    {
+    //если статья уже в избранном 
+     
+        echo CHtml::ajaxLink('<i class="icon-heart gray-a" 
+                                    rel="tooltip" 
+                                    title="убрать из избранного">'.$model->favourite_count.'</i>', 
+                    Yii::app()->createUrl('post/favorite'), array(
+                    'type' => 'POST',
+                    'data' => "js:'id-post='+$model->id",
+                    'success' => 'js:function(data) {postfavsuccess(data);}',
+                    'error' => 'js:function(data) {posterror();}',
+                ));
+            } else { 
+    //  если статья не в избранном
 
-            echo CHtml::ajaxLink('<i class="icon-heart black-a" 
-                                rel="tooltip" 
-                                title="добавить в избранное">'.$model->favourite_count.'</i>', 
-                Yii::app()->createUrl('post/favorite'), array(
-                'type' => 'POST',
-                'data' => "js:'id-post='+$model->id",
-                'success' => 'js:function(data) {postfavsuccess(data);}',
-                'error' => 'js:function(data) {posterror();}',
-            ));
+                echo CHtml::ajaxLink('<i class="icon-heart black-a" 
+                                    rel="tooltip" 
+                                    title="добавить в избранное">'.$model->favourite_count.'</i>', 
+                    Yii::app()->createUrl('post/favorite'), array(
+                    'type' => 'POST',
+                    'data' => "js:'id-post='+$model->id",
+                    'success' => 'js:function(data) {postfavsuccess(data);}',
+                    'error' => 'js:function(data) {posterror();}',
+                ));
+            }
         }
-    }
-    else echo '<i class="icon-heart" 
-                                rel="tooltip" 
-                                title="Чтобы добавить в избранное, авторизуйтесь"></i>';
+            else echo '<i class="icon-heart" 
+                                    rel="tooltip" 
+                                    title="Чтобы добавить в избранное, авторизуйтесь"></i>';
     ?>
-    <?php echo $model->favourite_count; ?>
 
